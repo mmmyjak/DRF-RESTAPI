d6bd11e0bdd457c7f752f741f3c7279876344cc1
mmmyjak <matim0@onet.pl>
2022-07-30 13:02:52 +0200

    added donethings view

--- drfrestapi/things/serializers.py
+++ drfrestapi/things/serializers.py
@@ -3,10 +3,7 @@ from .models import Category, Thing
 from django.contrib.auth.models import User
 
 
-class NameLinkField(serializers.RelatedField):
-    
-    def to_representation(self, value):
-        return '%s - %s' % (value, value.name)
+
 
 class CategorySerializer(serializers.ModelSerializer):
     things = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='things-detail')
@@ -32,8 +29,13 @@ class ThingSerializer(serializers.ModelSerializer):
             "url"
         )
 
+
+class NameBoolField(serializers.RelatedField):
+    def to_representation(self, value):
+        return '%s - DONE: %s' % (value, value.done)
+        
 class UserSerializer(serializers.ModelSerializer):
-    things = NameLinkField(many=True, read_only=True)
+    things = NameBoolField(many=True, read_only=True)
     class Meta:
         model = User
         fields = ['id', 'username', 'things']
--- drfrestapi/things/views.py
+++ drfrestapi/things/views.py
@@ -1,11 +1,6 @@
 from .models import Category, Thing
 from .serializers import ThingSerializer, UserSerializer, CategorySerializer
-# from django.http import Http404
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework import status
-# from rest_framework import mixins
-from rest_framework import generics, permissions, viewsets
+from rest_framework import permissions, viewsets
 from django.contrib.auth.models import User
 from .permissions import IsOwnerOrReadOnly
 
@@ -14,9 +9,15 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserSerializer
 
 class ThingViewSet(viewsets.ModelViewSet):
-    queryset = Thing.objects.all().filter(done=False)
+    
+    def get_queryset(self):
+        if self.action == 'list':
+            return Thing.objects.all().filter(done=False)
+        else:
+            return Thing.objects.all()
+
     serializer_class = ThingSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -28,99 +29,3 @@ class DoneThingViewSet(viewsets.ReadOnlyModelViewSet):
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
-
-# class ThingDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Thing.objects.all()
-#     serializer_class = ThingSerializer
-#     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-
-# class UserList(generics.ListAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-
-
-# class UserDetail(generics.RetrieveAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-
-# --------------- MIXINS --------------------------
-# class ThingsList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
-#     queryset = Things.objects.all()
-#     serializer_class = ThingsSerializer
-
-#     def get(self, request, *args, **kwargs):
-#         return self.list(request, *args, **kwargs)
-
-#     def post(self, request, *args, **kwargs):
-#         return self.create(request, *args, **kwargs)
-
-# class ThingsDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
-#     queryset = Things.objects.all()
-#     serializer_class = ThingsSerializer
-
-#     def get(self, request, *args, **kwargs):
-#         return self.retrieve(request, *args, **kwargs)
-
-#     def put(self, request, *args, **kwargs):
-#         return self.update(request, *args, **kwargs)
-
-#     def delete(self, request, *args, **kwargs):
-#         return self.destroy(request, *args, **kwargs)
-
-
-# --------------- APIVIEW --------------------------
-# class ThingsList(APIView):
-
-#     def get(self, request, format=None):
-#         things = Things.objects.all()
-#         serializer = ThingsSerializer(things, many=True)
-#         return Response(serializer.data)
-
-#     def post(self, request, format=None):
-#         serializer = ThingsSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class ThingsDetail(APIView):
-
-#     def get_object(self, pk):
-#         try:
-#             return Things.objects.get(pk=pk)
-#         except Things.DoesNotExist:
-#             raise Http404
-
-#     def get(self, request, pk, format=None):
-#         thing = self.get_object(pk)
-#         serializer = ThingsSerializer(thing)
-#         return Response(serializer.data)
-
-#     def put(self, request, pk, format=None):
-#         thing = self.get_object(pk)
-#         serializer = ThingsSerializer(thing, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-#     def delete(self, request, pk, format=None):
-#         thing = self.get_object(pk)
-#         thing.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-
-# class CategoryList(APIView):
-
-#     def get_object(self, cat):
-#         try:
-#             category = Category.objects.get(name=cat.lower())
-#             return Things.objects.all().filter(category=category.id)
-#         except (Things.DoesNotExist, Category.DoesNotExist):
-#             return Http404
-    
-#     def get(self, request, cat, format=None):
-#         things = self.get_object(cat)
-#         serializer = ThingsSerializer(things, many=True)
-#         return Response(serializer.data)
